eb52fa078d69bab7de69f5b18dbdad49c1857b2b
vincenttang1101 <tangtrinhquang@gmail.com>
2023-08-27 18:40:50 +0700

    coding-exercise-4 - Add Solution 2

--- coding-exercise-4/areThereDuplicates.js
+++ coding-exercise-4/areThereDuplicates.js
@@ -1,3 +1,4 @@
+/* Solution 1:
 function areThereDuplicates() {
     const collection = {}
     
@@ -10,7 +11,25 @@ function areThereDuplicates() {
     console.log(collection)
     return false
 }
+*/
+
+/* Solution 2: */
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort()
+
+  let start = 0
+  let next = 1
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true
+    }
+    start++
+    next++
+  }
+  return false
+}
 
 areThereDuplicates(1, 2, 3) // false
-areThereDuplicates(1, 2, 2) // true 
-areThereDuplicates('a', 'b', 'c', 'a') // true 
+areThereDuplicates(1, 2, 2) // true
+areThereDuplicates('a', 'b', 'c', 'a') // true
