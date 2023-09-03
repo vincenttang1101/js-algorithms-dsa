5111f86cdba998e0eb903062f98bba010ea52d27
vincenttang1101 <tangtrinhquang@gmail.com>
2023-09-03 17:48:37 +0700

    Frequency Counter - sameFrequency

--- coding-exercise-3/sameFrequency.js
+++ coding-exercise-3/sameFrequency.js
@@ -7,9 +7,7 @@ function sameFrequency(num1, num2) {
     const countNum2 = {}
 
     for (let i = 0; i < strNum1.length; i++) countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
-
     for (let j = 0; j < strNum2.length; j++) countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
-
     for (let key in countNum1) if (countNum1[key] !== countNum2[key]) return false
 
     return true
