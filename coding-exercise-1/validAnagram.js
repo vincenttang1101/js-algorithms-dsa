93fdc0b90bc5e5706c924b455dce6e6f94902097
vincenttang1101 <tangtrinhquang@gmail.com>
2023-09-03 16:54:54 +0700

    Frequency Counter - validAnagram

--- coding-exercise-1/validAnagram.js
+++ coding-exercise-1/validAnagram.js
@@ -1,15 +1,11 @@
 /* Solution 1:
 function validAnagram(str1, str2){
-    if (str1.length !== str2.length) {
-        return false
-    } 
+    if (str1.length !== str2.length)  return false
     
     for (let i=0; i<str1.length; i++) {
         const correctIndex = str2.indexOf(str1[i])
 
-        if (correctIndex === -1) {
-            return false
-        }
+        if (correctIndex === -1)  return false
         str2 = str2.slice(0, correctIndex) + str2.slice(correctIndex + 1);
     }
     return true
@@ -20,9 +16,7 @@ function validAnagram(str1, str2){
 function validAnagram(str1, str2) {
   let lookup = {}
 
-  if (str1.length !== str2.length) {
-    return false
-  }
+  if (str1.length !== str2.length) return false
 
   for (let i = 0; i < str1.length; i++) {
     let letter = str1[i]
