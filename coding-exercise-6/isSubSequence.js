7b814b27fcc15cbab7c184fd96e4210c4268fe53
vincenttang1101 <tangtrinhquang@gmail.com>
2023-09-04 13:54:23 +0700

    Multiple Pointers - isSubsequence

--- coding-exercise-6/isSubSequence.js
+++ coding-exercise-6/isSubSequence.js
@@ -1,3 +1,4 @@
+/* Solution 1:
 function isSubsequence(str1, str2) {
   var i = 0
   var j = 0
@@ -9,3 +10,17 @@ function isSubsequence(str1, str2) {
   }
   return false
 }
+*/
+
+/* Solution 2: */
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true
+  if (str2.length === 0) return false
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+  return isSubsequence(str1, str2.slice(1))
+}
+
+isSubsequence('hello', 'hello world') // true
+isSubsequence('sing', 'sting') // true
+isSubsequence('abc', 'abracadabra') // true
+isSubsequence('abc', 'acb') // false (order matters)
