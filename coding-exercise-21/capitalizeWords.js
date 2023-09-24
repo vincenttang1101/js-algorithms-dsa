eb85f1aedecd4859fc30906c687d052e525d2cfa
vincenttang1101 <tangtrinhquang@gmail.com>
2023-09-24 23:22:36 +0700

    Recursive - capitalizeWords

---
+++ coding-exercise-21/capitalizeWords.js
@@ -0,0 +1,11 @@
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()]
+  }
+  let res = capitalizeWords(array.slice(0, -1))
+  res.push(array.slice(array.length - 1)[0].toUpperCase())
+  return res
+}
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
