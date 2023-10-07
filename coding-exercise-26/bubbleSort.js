8923c7e88473b8c96284bf75d8cef9745a4bb0f1
vincenttang1101 <tangtrinhquang@gmail.com>
2023-10-07 14:50:02 +0700

    Searching Algorithms - bubbleSort

---
+++ coding-exercise-26/bubbleSort.js
@@ -0,0 +1,18 @@
+function swap(arr, idx1, idx2) {
+  return ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+}
+
+function bubbleSort(arr) {
+  let noSwaps
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1)
+      noSwaps = false
+    }
+    if (noSwaps) break
+  }
+  return arr
+}
+
+bubbleSort([4, 9, 2, 5, 3])
