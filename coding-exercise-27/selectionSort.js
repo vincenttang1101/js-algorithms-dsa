1be9aeee93211fd305642f9391977c5353212c48
vincenttang1101 <tangtrinhquang@gmail.com>
2023-10-11 10:16:05 +0700

    Sorting Algorithm - selectionSort

---
+++ coding-exercise-27/selectionSort.js
@@ -0,0 +1,16 @@
+function swap(arr, idx1, idx2) {
+  return ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) lowest = j
+    }
+    if (i !== lowest) swap(arr, i, lowest)
+  }
+  return arr
+}
+
+selectionSort([0, 2, 34, 22, 10, 19, 17])
