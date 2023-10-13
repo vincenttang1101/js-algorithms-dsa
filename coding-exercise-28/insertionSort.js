ee2c6e792900315e0863f8e73dde9c548d2387e8
vincenttang1101 <tangtrinhquang@gmail.com>
2023-10-13 17:07:28 +0700

    Sort Algorithm - insertionSort

---
+++ coding-exercise-28/insertionSort.js
@@ -0,0 +1,10 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i]
+    for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) arr[j + 1] = arr[j]
+    arr[j + 1] = currentValue
+  }
+  return arr
+}
+
+insertionSort([2, 1, 9, 76, 4])
